f9c262941ba1b23f94b0f5570d7a5aa88864e955
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-08-18 20:01:16 -0400

    lint now works

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,14 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-  prettier
-]);
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  prettier,
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts", "prisma/generated/**"],
+  },
+];
 
 export default eslintConfig;
