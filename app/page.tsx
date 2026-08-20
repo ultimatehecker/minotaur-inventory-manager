99c8cdd1f06afc355afcc63c932200f733218e5c
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-08-20 18:55:17 -0400

    now check the role type

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
                     <h1 className="text-xl">
                         Welcome, {session?.user.firstName} {session?.user.lastName}!
                     </h1>
-                    <p className="text-sm text-fg-muted">{session?.user.isAdmin ? "Administrator" : "Standard"}</p>
+                    <p className="text-sm text-fg-muted">{session?.user.role === "ADMINISTRATOR" ? "Administrator" : "Standard"}</p>
                     <form action={logout}>
                         <button type="submit" className="rounded-full bg-accent px-6 py-2 text-sm hover:bg-accent-hover transition-colors">
                             Logout
